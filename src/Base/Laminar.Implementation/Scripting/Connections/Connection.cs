eb87c8122f8e286e4bc3123b0237a61e25319a7a
Adam-Wilkinson <adwilko101@gmail.com>
2023-02-01 14:19:01 +0000

    Fixed nullability in connection

--- src/Base/Laminar.Implementation/Scripting/Connections/Connection.cs
+++ src/Base/Laminar.Implementation/Scripting/Connections/Connection.cs
@@ -4,23 +4,13 @@ using Laminar.PluginFramework.NodeSystem.Contracts.Connectors;
 
 namespace Laminar.Implementation.Scripting.Connections;
 
-//internal record class Connection(IOutputConnector OutputConnector, IInputConnector InputConnector) : IConnection
-//{
-//    public event EventHandler OnBroken;
-
-//    public void Break()
-//    {
-//        OnBroken?.Invoke(this, EventArgs.Empty);
-//    }
-//}
-
 internal class Connection : IConnection
 {
-    public IInputConnector InputConnector { get; init; }
+    public required IInputConnector InputConnector { get; init; }
 
-    public IOutputConnector OutputConnector { get; init; }
+    public required IOutputConnector OutputConnector { get; init; }
 
-    public event EventHandler OnBroken;
+    public event EventHandler? OnBroken;
 
     public void Break()
     {
