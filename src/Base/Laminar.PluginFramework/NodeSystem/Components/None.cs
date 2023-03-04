3a298f00eaa68087081830a242d5946749a41c8a
Adam-Wilkinson <adwilko101@gmail.com>
2023-03-04 16:14:13 +0000

    Added ManualNodeRow for more granular row control

---
+++ src/Base/Laminar.PluginFramework/NodeSystem/Components/None.cs
@@ -0,0 +1,8 @@
+﻿namespace Laminar.PluginFramework.NodeSystem.Components;
+
+/// <summary>
+/// Stub class used for null generic arguments
+/// </summary>
+public class None
+{
+}
--- src/Plugins/BasicFunctionality/Nodes/Maths/Arithmetic/SliderTest.cs
+++ src/Plugins/BasicFunctionality/Nodes/Maths/Arithmetic/SliderTest.cs
@@ -2,6 +2,7 @@
 using Laminar.PluginFramework.NodeSystem.Attributes;
 using Laminar.PluginFramework.NodeSystem.Components;
 using Laminar.PluginFramework.NodeSystem.IO.Value;
+using Laminar.PluginFramework.UserInterface;
 using Laminar.PluginFramework.UserInterface.UserInterfaceDefinitions;
 using static Laminar.PluginFramework.LaminarFactory;
 
@@ -9,11 +10,13 @@ namespace BasicFunctionality.Nodes.Maths.Arithmetic;
 
 public partial class SliderTest : INode
 {
-    [ShowInNode] readonly ValueOutputRow<double> Output = Component.ValueOutput("Slider Test", 2.0, isUserEditable: true, editor: new Slider(0, 4));
+    [ShowInNode] readonly ManualNodeRow<None, IDisplayValue, IValueOutput<double>> SliderRow = Component.ManualOutputRow(NodeIO.ValueInput("Test Slider", 2.0, new Slider(0, 4)).DisplayValue, NodeIO.ValueOutput("Test Slider Output", 0.0));
 
     public string NodeName { get; } = "Slider Test";
 
     public void Evaluate()
     {
+        Components
+        SliderRow.Output.Value = (double)SliderRow.Display.Value;
     }
 }
