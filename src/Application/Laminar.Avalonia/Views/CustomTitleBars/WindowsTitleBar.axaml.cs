1eee2d8f9c9305549552b6b57686c4a433170b22
Adam-Wilkinson <adwilko101@gmail.com>
2026-04-14 04:38:17 +0100

    First pass of slightly better title bar handling, but it's not perfect

--- src/Application/Laminar.Avalonia/Views/CustomTitleBars/WindowsTitleBar.axaml.cs
+++ src/Application/Laminar.Avalonia/Views/CustomTitleBars/WindowsTitleBar.axaml.cs
@@ -16,9 +16,9 @@ public partial class WindowsTitleBar : LaminarTitleBar
     {
         InitializeComponent();
 
-        MinimizeButton.Click += MinimizeWindow;
-        MaximizeButton.Click += MaximizeWindow;
-        CloseButton.Click += CloseWindow;
+        // MinimizeButton.Click += MinimizeWindow;
+        // MaximizeButton.Click += MaximizeWindow;
+        // CloseButton.Click += CloseWindow;
     }
 
     public Control LaminarButtonContent
@@ -27,29 +27,29 @@ public partial class WindowsTitleBar : LaminarTitleBar
         set => SetValue(LaminarButtonContentProperty, value);
     }
     
-    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-    {
-        if (VisualRoot is not Window hostWindow)
-        {
-            return;
-        }
-
-        hostWindow.GetObservable(Window.WindowStateProperty).Subscribe(new AnonymousObserver<WindowState>(s =>
-        {
-            if (s != WindowState.Maximized)
-            {
-                MaximizeIcon.Data = Geometry.Parse("M1,1 v-2 h-2 v2 z");
-                hostWindow.Padding = new Thickness(0, 0, 0, 0);
-                MaximizeToolTip.Content = "Maximize";
-            }
-            if (s == WindowState.Maximized)
-            {
-                MaximizeIcon.Data = Geometry.Parse("M2,1.5 h-0.5 v0.5 h0.5 z M2,1.8 h0.2 v-0.5 h-0.5 v0.2");
-                hostWindow.Padding = new Thickness(7, 7, 7, 7);
-                MaximizeToolTip.Content = "Restore Down";
-            }
-        }));
-    }
+    // protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    // {
+    //     if (VisualRoot is not Window hostWindow)
+    //     {
+    //         return;
+    //     }
+    //
+    //     hostWindow.GetObservable(Window.WindowStateProperty).Subscribe(new AnonymousObserver<WindowState>(s =>
+    //     {
+    //         if (s != WindowState.Maximized)
+    //         {
+    //             MaximizeIcon.Data = Geometry.Parse("M1,1 v-2 h-2 v2 z");
+    //             hostWindow.Padding = new Thickness(0, 0, 0, 0);
+    //             MaximizeToolTip.Content = "Maximize";
+    //         }
+    //         if (s == WindowState.Maximized)
+    //         {
+    //             MaximizeIcon.Data = Geometry.Parse("M2,1.5 h-0.5 v0.5 h0.5 z M2,1.8 h0.2 v-0.5 h-0.5 v0.2");
+    //             hostWindow.Padding = new Thickness(7, 7, 7, 7);
+    //             MaximizeToolTip.Content = "Restore Down";
+    //         }
+    //     }));
+    // }
 
     private void CloseWindow(object? sender, RoutedEventArgs e)
     {
