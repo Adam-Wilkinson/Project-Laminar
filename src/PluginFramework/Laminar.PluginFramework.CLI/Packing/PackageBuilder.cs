0aaeb5b7c231d3d719a9c3a418b4d5efad353c47
Adam Wilkinson <adwilko101@gmail.com>
2026-08-04 17:40:22 +0100

    Added initial json parsing and separate plugin.schema.json from manifest.schema.json

--- src/PluginFramework/Laminar.PluginFramework.CLI/Packing/PackageBuilder.cs
+++ src/PluginFramework/Laminar.PluginFramework.CLI/Packing/PackageBuilder.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using DotnetHelper;
 
 namespace Laminar.PluginFramework.CLI.Packing;
@@ -7,5 +8,24 @@ public static class PackageBuilder
     public static async Task BuildAsync(FileInfo csProjFile, DirectoryInfo? outputDirectory, CancellationToken ct)
     {
         await Dotnet.Publish(csProjFile.FullName);
+
+        if (csProjFile.Directory is not { } parentDir)
+        {
+            throw new InvalidOperationException($"Could not find parent directory for .csproj file '{csProjFile}'");
+        }
+
+        var pluginDir = Path.Combine(parentDir.FullName, "plugin.json");
+
+        if (!File.Exists(pluginDir))
+        {
+            throw new InvalidOperationException($"Could not find plugin.json file '{pluginDir}'");
+        }
+        
+        await using var pluginDataFile = File.OpenRead(pluginDir);
+        var pluginData = PluginData.Parse(pluginDataFile);
+
+        var pluginFullName = $"{(string)pluginData.Id}-{(int)pluginData.Version}";
+        
+        Debug.WriteLine($"Published plugin {pluginFullName}");
     }
 }
---
+++ src/PluginFramework/Laminar.PluginFramework.CLI/Packing/PluginData.cs
@@ -0,0 +1,6 @@
+using Corvus.Json;
+
+namespace Laminar.PluginFramework.CLI.Packing;
+
+[JsonSchemaTypeGenerator("../../plugin.schema.json")]
+public readonly partial struct PluginData;
